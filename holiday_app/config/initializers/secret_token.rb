32637251637999c81bb10d6323ffb07467d49fb0
James Hunt <ohthatjames@googlemail.com>
2012-06-14 13:13:29 +0100

    RRRRRRRRAAAAAAAAAIIIIILLLLLLLLSSSSSS

---
+++ holiday_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HolidayApp::Application.config.secret_token = 'f7e80fbde38d0990061a21cb978330152914c9bc0f30b874cd10199697e164424635624ec9045feeaa86a62ed5be5a62415196a41b650c204c98649fb9e62ad4'
