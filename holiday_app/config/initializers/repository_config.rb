83f0c8bfdd9739f7b905830826aea130f430fae6
James Hunt <ohthatjames@googlemail.com>
2012-06-15 09:15:35 +0100

    Standardise db name

--- holiday_app/config/initializers/repository_config.rb
+++ holiday_app/config/initializers/repository_config.rb
@@ -1,3 +1,3 @@
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "db/holiday_development")
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "db/holiday_development.sqlite3")
 
 Holiday::HolidayRepository.instance = Holiday::ActiveRecordHolidayRepository.new
