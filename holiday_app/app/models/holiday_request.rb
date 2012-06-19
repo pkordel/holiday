e4845a5f8b6cd5e8d266f91778b0a447f7d26dd1
James Hunt <ohthatjames@gmail.com>
2012-06-19 22:22:50 +0100

    Remove clunky ActiveModel attribute parser

--- holiday_app/app/models/holiday_request.rb
+++ holiday_app/app/models/holiday_request.rb
@@ -2,32 +2,16 @@ class HolidayRequest < OpenStruct
   extend ActiveModel::Naming
   include ActiveModel::Conversion
 
-  # Taken from http://pushandpop.blogspot.co.uk/2011/06/activemodel-object-with-multi-part.html
-  # There must be a better way of dealing with this. Where does ActiveRecord deal with casting?
-  def initialize( attrs = {} )
-    # Initialise OpenStruct first, so we can arbitrarily define crap.
-    super()
-    if !attrs.nil? then
-      dattrs = {}
-      attrs.each do |n, v|
-        if n.match( /^(.+)\(.+\)$/ ) then
-          an = Regexp.last_match[1]
-          dattrs[an] = [] if dattrs[an].nil?
-          dattrs[an] << { :n => n, :v => v }
-        else
-          send( "#{n}=", v)
-        end
-      end
-      dattrs.each do |k, v|
-        vs = v.sort_by{|hv| hv[:n] }.collect{|hv| hv[:v] }
-        p1 = vs[0]
-        p2 = ( vs[1].size() > 0 ? ( vs[1].size() == 1 ? "0#{vs[1]}" : vs[1] ) : "01" )
-        p3 = ( vs[2].size() > 0 ? ( vs[2].size() == 1 ? "0#{vs[2]}" : vs[2] ) : "01" )
-        dv = [ p1, p2, p3 ].join( "-" )
-        begin send( "#{k}=", Date.parse( dv ) ); rescue; end
-      end
+  def self.date_field(name)
+    define_method(name) do
+      parts = (1..3).to_a.map {|part| send("#{name}(#{part}i)")}
+      return nil if parts.any? {|part| part.nil?}
+      Date.parse(parts.join("-"))
     end
   end
+
+  date_field :starts_on
+  date_field :ends_on
 
   def persisted?
     false
